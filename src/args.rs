cd1db9cd6753fc309f61dc019d9097b5e24ad035
FruitSnaxx5752 <150543021+FruitSnaxx5752@users.noreply.github.com>
2024-05-14 18:03:39 -0600

    Finished adding options
    Added an option for
    Uppercase(-U,--Uppercase)
    Lowercase(-L, --Lowercase)
    Numeric(-N, --Numeric)
    Special(-S, --Special)
    characters

--- src/args.rs
+++ src/args.rs
@@ -2,17 +2,52 @@ use clap::Parser;
 
 #[derive(Parser, Debug, Default)]
 #[command(version, about, long_about)]
-pub struct Length{
-    ///Length of new password (Max length: 255)
+pub struct Args{
+    /// Length of new password (Max length: 255) 0 Will be Deault
     #[arg(short= 'l',long = "length", default_value_t = 15)]
     length: u8,
-
+    // Option for Lowercase charecters
+    #[arg(short= 'L', long = "Lowercase", default_value = "false")]
+    lowercase: bool,
+    // Option for Uppercase charecters
+    #[arg(short = 'U', long = "Uppercase", default_value = "false")]
+    uppercase: bool,
+    // Option for Numbers charecters
+    #[arg(short= 'N', long = "Numerical", default_value = "false")]
+    numerical: bool,
+    // Option for Special charecters
+    #[arg(short= 'S', long = "Specail", default_value = "false")]
+    special: bool,
 }
+
 pub fn length()-> u8{
-    let length: Length = Length::parse();
-    let mut len: u8 = length.length;
+    let args: Args = Args::parse();
+    let len: u8 = args.length;
+    return pass_len(len);
+}
+
+fn pass_len(mut len:u8) -> u8{
     if len == 0{
         len = 15;
+        return len;
+    }else{
+        return len;
     }
-    return len;
+}
+
+pub fn charlist() -> (bool, bool, bool, bool){
+    let args: Args = Args::parse();
+    let mut lowercase: bool = args.lowercase;
+    let mut uppercase: bool = args.uppercase;
+    let mut numeric: bool = args.numerical;
+    let mut special: bool = args.special;
+    if lowercase == false && uppercase == false && numeric == false && special == false{
+        lowercase = true;
+        uppercase = true;
+        numeric = true;
+        special = true;
+        return (lowercase, uppercase, numeric, special);
+    }else {
+        return (lowercase, uppercase, numeric, special);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use rand::{thread_rng, Rng};
 mod args;
 
 fn main(){
+
+    let char_list:(bool, bool, bool, bool) = args::charlist();
     let alpha_lower: Vec<char> = 
         vec!['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     let alpha_upper: Vec<char> = 
@@ -14,26 +16,31 @@ fn main(){
     let mut pass_length:u8 = args::length();
     let mut gen_password: Vec<char> = vec![];
     while pass_length > 0 {
-        let random_selection: i8 = thread_rng().gen_range(1..5);
-        if random_selection == 1 {
+        let random_selection: u8 = thread_rng().gen_range(1..5);
+        if random_selection == 1 && char_list.0 == true{
             let random_char_select = thread_rng().gen_range(0..alpha_lower.len().try_into().unwrap()); 
             let random_char: char = alpha_lower[random_char_select].clone();
             gen_password.push(random_char);
-        }else if random_selection == 2{
+            pass_length = pass_length -1;
+        }else if random_selection == 2 && char_list.1 == true{
             let random_char_select = thread_rng().gen_range(0..alpha_upper.len().try_into().unwrap());
             let random_char: char = alpha_upper[random_char_select].clone(); 
             gen_password.push(random_char);
-        }else if random_selection == 3 {
+            pass_length = pass_length -1;
+        }else if random_selection == 3 && char_list.2 == true{
             let random_char_select = thread_rng().gen_range(0..numeric.len().try_into().unwrap());
             let random_char: char = numeric[random_char_select].clone();
             gen_password.push(random_char);
-        }else if random_selection == 4 {
+            pass_length = pass_length -1;
+        }else if random_selection == 4 && char_list.3 == true{
             let random_char_select = thread_rng().gen_range(0..special.len().try_into().unwrap());
             let random_char: char = special[random_char_select].clone(); 
             gen_password.push(random_char);
+            pass_length = pass_length -1;
         }
-        pass_length = pass_length -1;
     }
+
     let password: String = gen_password.clone().into_iter().map(|i| i.to_string()).collect::<String>();
-    println!("{}",password)
+    println!("{}",password);
+
 }
